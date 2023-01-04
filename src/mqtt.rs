63220ef2ce13f307e94e1643c6439c5b50a54266
David Weis <davidmichaelweis@gmail.com>
2023-01-04 00:36:54 +0000

    set clean session

--- src/mqtt.rs
+++ src/mqtt.rs
@@ -1,15 +1,17 @@
 use crate::configuration::MqttSettings;
+use anyhow::{Context, Result};
 use rumqttc::{self, AsyncClient, MqttOptions, Transport};
 use rustls::ClientConfig;
 use secrecy::ExposeSecret;
 
-pub fn create_mqtt_client(config: &MqttSettings) -> anyhow::Result<AsyncClient> {
-    let mut mqttoptions = MqttOptions::new(&config.client_id, &config.host, config.port);
-    mqttoptions.set_keep_alive(std::time::Duration::from_secs(5));
-    mqttoptions.set_credentials(&config.username, config.password.expose_secret());
+pub fn create_mqtt_client(config: &MqttSettings) -> Result<AsyncClient> {
+    let mut mqtt_options = MqttOptions::new(&config.client_id, &config.host, config.port);
+    mqtt_options.set_clean_session(true);
+    mqtt_options.set_keep_alive(std::time::Duration::from_secs(5));
+    mqtt_options.set_credentials(&config.username, config.password.expose_secret());
 
     let mut root_cert_store = rustls::RootCertStore::empty();
-    for cert in rustls_native_certs::load_native_certs().expect("could not load platform certs") {
+    for cert in rustls_native_certs::load_native_certs().context("could not load platform certs")? {
         root_cert_store.add(&rustls::Certificate(cert.0))?;
     }
 
@@ -18,9 +20,9 @@ pub fn create_mqtt_client(config: &MqttSettings) -> anyhow::Result<AsyncClient> 
         .with_root_certificates(root_cert_store)
         .with_no_client_auth();
 
-    mqttoptions.set_transport(Transport::tls_with_config(client_config.into()));
+    mqtt_options.set_transport(Transport::tls_with_config(client_config.into()));
 
-    let (client, mut eventloop) = AsyncClient::new(mqttoptions, 10);
+    let (client, mut eventloop) = AsyncClient::new(mqtt_options, 10);
 
     tokio::spawn(async move {
         loop {
